6f3b3231a050432c9fffda82b51e78f07cc002c0
David Freifeld <freifeld.david@gmail.com>
2022-03-03 21:17:38 -0800

    Add tweaks to demo and README

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -7,42 +7,62 @@ namespace py = pybind11;
 #include "fold.cuh"
 
 auto get_residues(Protein p) {
-	std::vector<std::tuple<Amino, Eigen::Vector3i>> out;
-	for (int i = 0; i < p.sz; i++) {
-		out.push_back({p.sequence[i], p.positions[i]});
+    std::vector<std::tuple<Amino, Eigen::Vector3i>> out;
+    for (int i = 0; i < p.sz; i++) {
+        out.push_back({p.sequence[i], p.positions[i]});
+    }
+    return out;
+}
+
+auto check_residue_local(Protein protein, Eigen::Vector3i pos) {
+	for (int i = 0; i < protein.sz; i++) {
+		if (protein.positions[i] == pos) return false;
 	}
-	return out;
+	return true;
+}
+
+auto get_cost_local(Protein protein) {
+    int cost = 0;
+	const Eigen::Vector3i unit[3] = {{0,0,1}, {0,1,0}, {1,0,0}};
+	for (int i = 0; i < protein.sz; i++) {
+		for (int i = 0; i < 3; i++) {
+			if (check_residue_local(protein, protein.positions[i] + unit[i])) cost++;
+			if (check_residue_local(protein, protein.positions[i] - unit[i])) cost++;
+		}
+	}
+    return cost;
 }
 
 PYBIND11_MODULE(fold, m) {
-	m.doc() = "Cheap low-resolution lattice protein folding simulation code.";
-	py::enum_<Amino>(m, "Amino")
-		.value("Alanine", Amino::Alanine)
-		.value("Arganine", Amino::Arganine)
-		.value("Asparagine", Amino::Asparagine)
-		.value("AsparticAcid", Amino::AsparticAcid)
-		.value("Cysteine", Amino::Cysteine)
-		.value("Glutamine", Amino::Glutamine)
-		.value("GlutamicAcid", Amino::GlutamicAcid)
-		.value("Glycine", Amino::Glycine)
-		.value("Histidine", Amino::Histidine)
-		.value("Isoleucine", Amino::Isoleucine)
-		.value("Leucine", Amino::Leucine)
-		.value("Lysine", Amino::Lysine)
-		.value("Methionine", Amino::Methionine)
-		.value("Phenylalanine", Amino::Phenylalanine)
-		.value("Proline", Amino::Proline)
-		.value("Serine", Amino::Serine)
-		.value("Threonine", Amino::Threonine)
-		.value("Tryptophan", Amino::Tryptophan)
-		.value("Tyrosine", Amino::Tyrosine)
-		.value("Valine", Amino::Valine)
-		.export_values();
-	py::class_<Sequence>(m, "Sequence")
-		.def(py::init<const std::string&>());
-	py::class_<Protein>(m, "RawProtein")
-		.def(py::init<Sequence>())
-		.def_static("random", &Protein::random);	
-	m.def("anneal_multistart_singlestrat", &anneal_multistart_singlestrat);
-	m.def("get_residues", &get_residues);
+    m.doc() = "Cheap low-resolution lattice protein folding simulation code.";
+    py::enum_<Amino>(m, "Amino")
+        .value("Alanine", Amino::Alanine)
+        .value("Arganine", Amino::Arganine)
+        .value("Asparagine", Amino::Asparagine)
+        .value("AsparticAcid", Amino::AsparticAcid)
+        .value("Cysteine", Amino::Cysteine)
+        .value("Glutamine", Amino::Glutamine)
+        .value("GlutamicAcid", Amino::GlutamicAcid)
+        .value("Glycine", Amino::Glycine)
+        .value("Histidine", Amino::Histidine)
+        .value("Isoleucine", Amino::Isoleucine)
+        .value("Leucine", Amino::Leucine)
+        .value("Lysine", Amino::Lysine)
+        .value("Methionine", Amino::Methionine)
+        .value("Phenylalanine", Amino::Phenylalanine)
+        .value("Proline", Amino::Proline)
+        .value("Serine", Amino::Serine)
+        .value("Threonine", Amino::Threonine)
+        .value("Tryptophan", Amino::Tryptophan)
+        .value("Tyrosine", Amino::Tyrosine)
+        .value("Valine", Amino::Valine)
+        .export_values();
+    py::class_<Sequence>(m, "Sequence")
+        .def(py::init<const std::string&>());
+    py::class_<Protein>(m, "RawProtein")
+        .def(py::init<Sequence>())
+        .def_static("random", &Protein::random);
+    m.def("anneal_multistart_singlestrat", &anneal_multistart_singlestrat);
+    m.def("get_residues", &get_residues);
+    m.def("get_cost", &get_cost_local);
 }
--- vis.py
+++ vis.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
 import fold
 
-seq = fold.Sequence("MSVADDDLGSLQGHIRRTLRSIHNLPYFRYTRGPTERADMSRALKEFIYRYLYFVISNSGENLPTLFNAHPKQKLSNPELTVFPDSLEDAVDIDKITSQQTIPFYKIDESRIGDVHKHTGRNCGRKFKIGEPLYRCHE")
+seq = fold.Sequence("MSVADDDLGSLQ")
 
 fig, axs = plt.subplots(10,8, subplot_kw=dict(projection='3d'))
 gs = axs[8, 0].get_gridspec()
@@ -15,12 +15,16 @@ for i in axs[8:, 0:]:
     for ax in i:
         ax.remove()
 axbig = fig.add_subplot(gs[8:, 0:])
+costs = []
+
 def animate(i):
+    this_costs = []
     for a in axs[:8]:
         for j in a:
             j.clear()
             j.axis("off")
             p = fold.RawProtein.random(seq)
+            this_costs.append(fold.get_cost(p))
             residues = fold.get_residues(p)
             aminos, pos = zip(*residues)
             x, y, z = zip(*pos)
@@ -29,10 +33,12 @@ def animate(i):
             j.add_collection(cc)
             j.scatter(x, y, z, s=10)
             j.set_box_aspect((np.ptp(x), np.ptp(y), np.ptp(z)))
-
+    
+    costs.append(sum(this_costs)/len(this_costs))
     axbig.clear()
-    axbig.plot([1,2,3], [4,5,6])        
+    axbig.plot(costs)
         
     plt.figtext(0.5, 0.9, "Initializing", wrap=True, horizontalalignment='center', fontsize=25, family="monospace", fontweight="bold")
 ani = animation.FuncAnimation(fig, animate, interval=1)
+
 plt.show()
